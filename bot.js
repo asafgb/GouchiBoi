a7810c1c69945134174841193fb812faa4507309
asafgb <asafshazar@gmail.com>
2018-05-19 14:02:03 +0300

    commit

--- bot.js
+++ bot.js
@@ -1,15 +1,25 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const commando = require('discord.js-commando');
+const bot =  new commando.Client();
 
-client.on('ready', () => {
+bot.registry.registerGroup('random','Random');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+
+
+//const Discord = require('discord.js');
+//const client = new Discord.Client();
+
+/*client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
     if (message.content === 'ping') {
-    	message.reply('pong');
+        //message.reply('pong');
+        message.channel.sendMessage("pong")
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+client.login("NDQ3MzQyNDAwMzI3Nzc4MzE0.DeGLmA.s7NzYoY_5AQ5GG31gZRCoBlhC0Y");//process.env.BOT_TOKEN
+*/
